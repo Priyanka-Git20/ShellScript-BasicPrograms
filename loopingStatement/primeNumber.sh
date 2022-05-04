40b304876089bff216fbbb6bad75d68835e8f51d
priyanka <priyanka.chavan1408@gmail.com>
2022-05-04 19:24:40 +0530

    [Priyanka]Add:Checking given number is prime or not.

---
+++ loopingStatement/primeNumber.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+read -p "Enter a Number n: " n
+echo "number: "$n
+isPrime=1
+for ((i=2;i<$n;i++))
+do 
+ if [ $((n%i)) -eq 0 ]
+  then 
+   isPrime=0
+   break
+ fi
+done
+
+if [ $isPrime -eq 1 ]
+ then
+  echo "it is a prime Number"
+else
+  echo "it is not a prime no "
+fi
